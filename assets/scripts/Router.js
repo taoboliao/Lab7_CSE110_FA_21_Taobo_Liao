c2d82f43d88e51c0fe204e519bb6b697b08dce6a
taoboliao <taliao@ucsd.edu>
2021-11-07 21:09:50 -0800

    update main and router

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,8 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +67,19 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+     if (this[page] == undefined) {
+      console.error('Page not found error');
+      return;
+    }
+    var hash;
+    if(page == 'home'){
+      hash = '';
+    }else{
+      hash = '#{page}';
+    }
+    if(!statePopped && window.location.hash != hash){
+      history.pushState({page}, document.title, '/${hash}');
+    }
+    this[page]();
   }
 }
